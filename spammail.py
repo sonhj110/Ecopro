ddd21120855d42d8218f3ff292f609a82172e427
hyojung <sonhj10318@gmail.com>
2023-10-09 21:57:41 +0900

    나이브베일즈 스팸메일분류 수정

--- spammail.py
+++ spammail.py
@@ -14,13 +14,10 @@ test = ['free lottery']
 
 
 # 훈련데이터 단어 단위로 끊어주기
-input_word = np.array(training_set)
-
 wordlist = []
 
-for w in input_word[:, 0] :
-  for i in range(len(w.split(' '))) :
-    wordlist.append(w.split(' ')[i])
+for mail in training_set :
+  wordlist.extend(mail[0].split(' '))
 
 wordlist = list(set(wordlist))
 wordlist.sort()
@@ -28,50 +25,41 @@ wordlist.sort()
 
 
 # 해당 단어를 포함한 메일의 정상빈도 스팸빈도
-norm = 0
-spam = 0
-freq = []
-
-totalSpam = 0
+wordDic = {}
 totalNorm = 0
+totalSpam = 0
 
 for word in wordlist :
-  norm = 0
   spam = 0
-  for main in input_word :
-    if word in main[0] :
-      if main[1] == '1' :
+  norm = 0
+
+  for train in training_set :
+    if word in train[0] :
+      if train[1] == 1 :
         spam += 1
       else :
         norm += 1
     else :
       pass
-  totalSpam += spam
+
   totalNorm += norm
-  freq.append([word, spam, norm])
+  totalSpam += spam
+  wordDic.setdefault(word, [spam, norm])
 
-print(freq)
-print(totalSpam, totalNorm)
-freq = np.array(freq)
-
-
-
-# 라플레이스 스무딩 적용한 확률 구하는 함수 정의
-def p(x, y) :
-  k = 0.5
-  return round((k + x) / (2*k + y) * 100, 4)
+wordDic
 
 
 
 # 해당 단어가 포함됐을대 스팸/정상일 조건부 확률 구하기
 # P(A|B) = P(A교집합B) / P(B)
-wordPlist = []
+Pdic = {}
 
-for wordP in freq :
-  # print(wordP)
-  wNorm = p(int(wordP[2]), totalNorm)
-  wSpam = p(int(wordP[1]), totalSpam)
-  wordPlist.append([wordP[0], wSpam, wNorm])
+for key, val in wordDic.items() :  # .items() 딕셔너리의 키, 값 모두 불러오는 메서드. 이거 없으면 for문으로 불러와지지 않음
+  wSpam = (0.5 + val[0]) / (0.5*2 + totalSpam)
+  wNorm = (0.5 + val[1]) / (0.5*2 + totalNorm)
+  Pdic.setdefault(key, [wSpam, wNorm])
+
+Pdic
 
 
 
@@ -79,35 +67,44 @@ for wordP in freq :
 import math
 
 def logP(x, y) :
-  k = 0.5
-  result = (k + x) / (2*k + y)
-  return round(math.log(result), 4)
-
-logP(1,3)
+  k = 0.5   # 라플레이스 스무딩
+  return math.log((k + x) / (2*k + y))
 
 
 
 # 조건부 확률에 로그 적용한 값 구하기
-wordPlist = []
+Pdic = {}
 
-for wordP in freq :
-  # print(wordP)
-  wNorm = p(int(wordP[2]), totalNorm)
-  wSpam = p(int(wordP[1]), totalSpam)
-  wNormLog = logP(int(wordP[2]), totalNorm)
-  wSpamLog = logP(int(wordP[1]), totalSpam)
-  wordPlist.append([wordP[0], wSpam, wNorm, wSpamLog, wNormLog])
+for key, val in wordDic.items() :  # .items() 딕셔너리의 키, 값 모두 불러오는 메서드. 이거 없으면 for문으로 불러와지지 않음
+  wSpam = logP(val[0], totalSpam)
+  wNorm = logP(val[1], totalNorm)
+  Pdic.setdefault(key, [wSpam, wNorm])
 
+Pdic
+
+
+
+
+# 테스트 데이터 분할
+testdata = test[0].split(' ')
 
 
 
 # 테스트 데이터 실험
-free = wordPlist[2]
-lottery = wordPlist[4]
+logTestSpam = logP(totalSpam, totalSpam + totalNorm)
+logTestNorm = logP(totalNorm, totalSpam + totalNorm)
 
-_PofNorm = math.exp(free[4]+lottery[4]+logP(totalNorm, totalNorm+totalSpam))*100
-_PofSpam = math.exp(free[3]+lottery[3]+logP(totalSpam, totalNorm+totalSpam))*100
+for testword in testdata :   # 테스트 데이터 인풋하기
+  logTestSpam += Pdic[testword][0]
+  logTestNorm += Pdic[testword][1]
 
-PofNorm = round((_PofNorm / _PofNorm + _PofSpam), 2)
-PofSpam = round((_PofSpam / _PofNorm + _PofSpam), 2)
-print(PofNorm, PofSpam)
+testSpam = math.exp(logTestSpam)*100
+testNorm = math.exp(logTestNorm)*100
+# print(testSpam, testNorm)
+
+
+
+# free와 lottery가 들어간 메일이 스팸일 확률, 정상일 확률
+Pspam = round((testSpam / (testSpam + testNorm)), 2)
+Pnorm = round((testNorm / (testSpam + testNorm)), 2)
+print(Pspam, Pnorm)
